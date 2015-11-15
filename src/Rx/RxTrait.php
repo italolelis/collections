fba8cef9d3bd32a0eba1734d0f96ffd31f558a7f
Ítalo Lelis de Vietro <italolelis@gmail.com>
2015-11-15 17:37:04 +0100

    Removing unnecessary methods

--- src/Rx/RxTrait.php
+++ src/Rx/RxTrait.php
@@ -5,66 +5,15 @@ namespace Collections\Rx;
 
 use CallbackFilterIterator;
 use Collections\ArrayList;
-use Collections\CollectionInterface;
 use Collections\Dictionary;
-use Collections\ExtractTrait;
 use Collections\Iterator\ExtractIterator;
 use Collections\Iterator\InsertIterator;
-use Collections\Iterator\ReplaceIterator;
+use Collections\Traits\ExtractTrait;
 use LimitIterator;
 
 trait RxTrait
 {
     use ExtractTrait;
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function each(callable $callable)
-    {
-        foreach ($this->getIterator() as $k => $v) {
-            $callable($v, $k);
-        }
-
-        return $this;
-    }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function map(callable $callable)
-    {
-        return $this->iteratorToCollection(new ReplaceIterator($this->getIterator(), $callable));
-    }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function filter(callable $callable)
-    {
-        return $this->iteratorToCollection(new CallbackFilterIterator($this->getIterator(), $callable));
-    }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function take($size = 1, $from = 0)
-    {
-        return $this->iteratorToCollection(new LimitIterator($this->getIterator(), $from, $size));
-    }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function slice($start, $length = null)
-    {
-        return $this->iteratorToCollection(array_slice($this->getIterator(), $start, $length));
-    }
 
     /**
      * {@inheritDoc}
@@ -80,63 +29,6 @@ trait RxTrait
     }
 
     /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function every(callable $callable)
-    {
-        foreach ($this->getIterator() as $key => $value) {
-            if (!$callable($value, $key)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function some(callable $callable)
-    {
-        foreach ($this->getIterator() as $key => $value) {
-            if ($callable($value, $key) === true) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Iteratively reduce the collection to a single value using a callback function.
-     *
-     * @param callable $callable The callable used for reduce.
-     * @param int $zero If the optional initial is available, it will be used at the beginning of the process,
-     * or as a final result in case the array is empty.
-     * @return $this A collection with the results of the filter operation.
-     */
-    public function reduce(callable $callable, $zero = null)
-    {
-        $isFirst = false;
-        if (func_num_args() < 2) {
-            $isFirst = true;
-        }
-        $result = $zero;
-        foreach ($this->getIterator() as $k => $value) {
-            if ($isFirst) {
-                $result = $value;
-                $isFirst = false;
-                continue;
-            }
-            $result = $callable($result, $value, $k);
-        }
-
-        return $result;
-    }
-
-    /**
      * @param $matcher
      * @return $this
      */
@@ -144,16 +36,6 @@ trait RxTrait
     {
         return $this->iteratorToCollection(new ExtractIterator($this, $matcher));
     }
-
-    /**
-     * {@inheritDoc}
-     * @return $this
-     */
-    public function sample($size = 10)
-    {
-        return $this->iteratorToCollection(new LimitIterator($this->shuffle()->getIterator(), 0, $size));
-    }
-
 
     /**
      * {@inheritDoc}
