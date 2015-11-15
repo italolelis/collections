f9fe2fd9844aa460e63ebdaeb8894403ddd8dd74
Ítalo Lelis de Vietro <italolelis@gmail.com>
2015-11-15 17:40:33 +0100

    New traits for different types

--- src/Traits/StrictIterableTrait.php
+++ src/Traits/StrictIterableTrait.php
@@ -2,45 +2,170 @@
 
 namespace Collections\Traits;
 
-use Collections\ArrayList;
-use Collections\Immutable\ImmArrayList;
-use Collections\Immutable\ImmSet;
-
 trait StrictIterableTrait
 {
+    use ExtractTrait;
+
     public function toArray()
     {
-        return $this->getIterator()->toArray();
+        $arr = [];
+        foreach ($this as $value) {
+            if ($value instanceof \Iterable) {
+                $arr[] = $value->toArray();
+            } else {
+                $arr[] = $value;
+            }
+        }
+
+        return $arr;
     }
 
-    public function toValuesArray()
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function map(callable $callable)
     {
-        return array_values($this->toArray());
+        $res = new static();
+        foreach ($this as $v) {
+            $res[] = $callable($v);
+        }
+
+        return $res;
     }
 
-    public function toVector()
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function filter(callable $callable)
     {
-        return new ArrayList($this->getIterator());
+        $res = new static();
+        foreach ($this as $v) {
+            if ($callable($v)) {
+                $res[] = $v;
+            }
+        }
+
+        return $res;
     }
 
-    public function toImmVector()
+    public function zip($iterable)
     {
-        return new ImmArrayList($this->getIterator());
+        $res = new static();
+        $it = $iterable->getIterator();
+        foreach ($this as $v) {
+            if (!$it->valid()) {
+                break;
+            }
+            $res[] = new Pair($v, $it->current());
+            $it->next();
+        }
+
+        return $res;
     }
 
-    public function toSet()
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function take($size = 1)
     {
-        // TODO: Implement toSet() method.
+        $res = new static();
+
+        if ($size <= 0) {
+            return $res;
+        }
+
+        foreach ($this as $v) {
+            $res[] = $v;
+            if (--$size === 0) {
+                break;
+            }
+        }
+
+        return $res;
     }
 
-    public function toImmSet()
+    public function takeWhile(callable $callable)
     {
-        return new ImmSet($this->getIterator());
+        $res = new static();
+        foreach ($this as $v) {
+            if (!$callable($v)) {
+                break;
+            }
+            $res[] = $v;
+        }
+
+        return $res;
     }
 
-    public function lazy()
+    public function skip($n)
     {
-        // TODO: Implement lazy() method.
+        $res = new static();
+        foreach ($this as $v) {
+            if ($n <= 0) {
+                $res[] = $v;
+            } else {
+                --$n;
+            }
+        }
+
+        return $res;
+    }
+
+    public function skipWhile(callable $callable)
+    {
+        $res = new static();
+        $skip = true;
+        foreach ($this as $v) {
+            if ($skip) {
+                if ($callable($v)) {
+                    continue;
+                }
+                $skip = false;
+            }
+            $res[] = $v;
+        }
+
+        return $res;
+    }
+
+    public function slice($start, $length)
+    {
+        $res = new static();
+        if ($length <= 0) {
+            return $res;
+        }
+        foreach ($this as $v) {
+            if ($start !== 0) {
+                --$start;
+                continue;
+            }
+            $res[] = $v;
+            if (--$length === 0) {
+                break;
+            }
+        }
+
+        return $res;
+    }
+
+    public function concat(\Traversable $iterable)
+    {
+        $res = [];
+
+        foreach ($this as $v) {
+            $res[] = $v;
+        }
+
+        foreach ($iterable as $v) {
+            $res[] = $v;
+        }
+        $this->container = $res;
+
+        return $this;
     }
 
     /**
@@ -49,9 +174,11 @@ trait StrictIterableTrait
      */
     public function first()
     {
-        $result = $this->toArray();
+        foreach ($this as $v) {
+            return $v;
+        }
 
-        return array_shift($result);
+        return null;
     }
 
     /**
@@ -65,8 +192,82 @@ trait StrictIterableTrait
         return array_pop($result);
     }
 
-    public function skip($n)
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function each(callable $callable)
     {
+        foreach ($this as $v) {
+            $callable($v);
+        }
 
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function every(callable $callable)
+    {
+        foreach ($this as $key => $value) {
+            if (!$callable($value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function some(callable $callable)
+    {
+        foreach ($this as $key => $value) {
+            if ($callable($value) === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Iteratively reduce the collection to a single value using a callback function.
+     *
+     * @param callable $callable The callable used for reduce.
+     * @param int $zero If the optional initial is available, it will be used at the beginning of the process,
+     * or as a final result in case the array is empty.
+     * @return $this A collection with the results of the filter operation.
+     */
+    public function reduce(callable $callable, $zero = null)
+    {
+        $isFirst = false;
+        if (func_num_args() < 2) {
+            $isFirst = true;
+        }
+        $result = $zero;
+        foreach ($this as $k => $value) {
+            if ($isFirst) {
+                $result = $value;
+                $isFirst = false;
+                continue;
+            }
+            $result = $callable($result, $value);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function match(array $conditions)
+    {
+        return $this->filter($this->createMatcherFilter($conditions));
     }
 }
--- src/Traits/StrictKeyedIterableTrait.php
+++ src/Traits/StrictKeyedIterableTrait.php
@@ -2,26 +2,298 @@
 
 namespace Collections\Traits;
 
+use Collections\ArrayList;
 use Collections\Dictionary;
-use Collections\Immutable\ImmDictionary;
 
 trait StrictKeyedIterableTrait
 {
-    public function toMap()
+    public function toArray()
     {
-        return new Dictionary($this->getIterator());
+        $arr = [];
+        foreach ($this as $key => $value) {
+            if ($value instanceof \Iterable) {
+                $arr[$key] = $value->toArray();
+            } else {
+                $arr[$key] = $value;
+            }
+        }
+
+        return $arr;
     }
 
-    public function toImmMap()
+    public function values()
     {
-        return new ImmDictionary($this->getIterator());
+        return new ArrayList($this);
+    }
+
+    public function keys()
+    {
+        $res = new ArrayList();
+        foreach ($this as $k => $_) {
+            $res[] = $k;
+        }
+
+        return $res;
+    }
+
+    public function map(callable $callback)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            $res[$k] = $callback($v);
+        }
+
+        return $res;
+    }
+
+    public function mapWithKey($callback)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            $res[$k] = $callback($k, $v);
+        }
+
+        return $res;
+    }
+
+    public function filter(callable $callback)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            if ($callback($v)) {
+                $res[$k] = $v;
+            }
+        }
+
+        return $res;
+    }
+
+    public function filterWithKey($callback)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            if ($callback($k, $v)) {
+                $res[$k] = $v;
+            }
+        }
+
+        return $res;
+    }
+
+    public function zip($iterable)
+    {
+        $res = new Dictionary();
+        $it = $iterable->getIterator();
+        foreach ($this as $k => $v) {
+            if (!$it->valid()) {
+                break;
+            }
+            $res[$k] = new Pair($v, $it->current());
+            $it->next();
+        }
+
+        return $res;
+    }
+
+    public function take($size = 1)
+    {
+        $res = new Dictionary();
+        if ($size <= 0) {
+            return $res;
+        }
+        foreach ($this as $k => $v) {
+            $res[$k] = $v;
+            if (--$size === 0) {
+                break;
+            }
+        }
+
+        return $res;
+    }
+
+    public function takeWhile($fn)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            if (!$fn($v)) {
+                break;
+            }
+            $res[$k] = $v;
+        }
+
+        return $res;
+    }
+
+    public function skip($n)
+    {
+        $res = new Dictionary();
+        foreach ($this as $k => $v) {
+            if ($n <= 0) {
+                $res[$k] = $v;
+            } else {
+                --$n;
+            }
+        }
+
+        return $res;
+    }
+
+    public function skipWhile($fn)
+    {
+        $res = new Dictionary();
+        $skip = true;
+        foreach ($this as $k => $v) {
+            if ($skip) {
+                if ($fn($v)) {
+                    continue;
+                }
+                $skip = false;
+            }
+            $res[$k] = $v;
+        }
+
+        return $res;
+    }
+
+    public function slice($start, $lenght)
+    {
+        $res = new Dictionary();
+        if ($lenght <= 0) {
+            return $res;
+        }
+        foreach ($this as $k => $v) {
+            if ($start !== 0) {
+                --$start;
+                continue;
+            }
+            $res[$k] = $v;
+            if (--$lenght === 0) {
+                break;
+            }
+        }
+
+        return $res;
+    }
+
+    public function concat(\Traversable $iterable)
+    {
+        $res = [];
+
+        foreach ($this as $k => $v) {
+            $res[$k] = $v;
+        }
+
+        foreach ($iterable as $k => $v) {
+            $res[$k] = $v;
+        }
+        $this->container = $res;
+
+        return $this;
+    }
+
+    public function first()
+    {
+        foreach ($this as $v) {
+            return $v;
+        }
+
+        return null;
+    }
+
+    public function firstKey()
+    {
+        foreach ($this as $k => $_) {
+            return $k;
+        }
+
+        return null;
+    }
+
+    public function last()
+    {
+        $v = null;
+        foreach ($this as $v) {
+        }
+
+        return $v;
+    }
+
+    public function lastKey()
+    {
+        $k = null;
+        foreach ($this as $k => $_) {
+        }
+
+        return $k;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
+     * @return $this
      */
-    public function toKeysArray()
+    public function each(callable $callable)
     {
-        return $this->getIterator()->keys()->toArray();
+        foreach ($this as $k => $v) {
+            $callable($v, $k);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function every(callable $callable)
+    {
+        foreach ($this as $key => $value) {
+            if (!$callable($value, $key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return $this
+     */
+    public function some(callable $callable)
+    {
+        foreach ($this as $key => $value) {
+            if ($callable($value, $key) === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Iteratively reduce the collection to a single value using a callback function.
+     *
+     * @param callable $callable The callable used for reduce.
+     * @param int $zero If the optional initial is available, it will be used at the beginning of the process,
+     * or as a final result in case the array is empty.
+     * @return $this A collection with the results of the filter operation.
+     */
+    public function reduce(callable $callable, $zero = null)
+    {
+        $isFirst = false;
+        if (func_num_args() < 2) {
+            $isFirst = true;
+        }
+        $result = $zero;
+        foreach ($this as $k => $value) {
+            if ($isFirst) {
+                $result = $value;
+                $isFirst = false;
+                continue;
+            }
+            $result = $callable($result, $value, $k);
+        }
+
+        return $result;
     }
 }
