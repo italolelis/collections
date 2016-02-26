fc3278af7dfbc0806b3db71442b08a9fbb61b44b
Ítalo Lelis <italolelis@gmail.com>
2016-02-26 12:07:10 +0100

    Added missing methods

--- src/KeyedIterable.php
+++ src/KeyedIterable.php
@@ -5,6 +5,37 @@ namespace Collections;
 interface KeyedIterable extends KeyedTraversable, Iterable
 {
     /**
+     * Returns an `KeyedIterable` containing the values after an operation has been
+     * applied to each value in the current `KeyedIterable`.
+     *
+     * Every value in the current `KeyedIterable` is affected by a call to `mapWithKey()`,
+     * unlike `filterWithKey()` where only values that meet a certain criteria are
+     * affected.
+     *
+     * @param $fn - The callback containing the operation to apply to the
+     *              `KeyedIterable` values.
+     *
+     * @return KeyedIterable - an `KeyedIterable` containing the values after a user-specified
+     *           operation is applied.
+     */
+    public function mapWithKey($callback);
+
+    /**
+     * Returns an `KeyedIterable` containing the values of the current `KeyedIterable` that
+     * meet a supplied condition.
+     *
+     * Only values that meet a certain criteria are affected by a call to
+     * `filterWithKey()`, while all values are affected by a call to `mapWithKey()`.
+     *
+     * @param $fn - The callback containing the condition to apply to the
+     *              `KeyedIterable` values.
+     *
+     * @return KeyedIterable - an `KeyedIterable` containing the values after a user-specified
+     *           condition is applied.
+     */
+    public function filterWithKey($callback);
+
+    /**
      * Returns an array whose values are the keys from the ICollection.
      * @return array
      */
